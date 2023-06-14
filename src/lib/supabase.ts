36182c706c8a45148d3808ed00eb98a46c7d8722
MacDevCore <dechmann@datora.de>
2023-06-14 05:52:55 +0200

    f

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -5,7 +5,4 @@ const supabaseAnonKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFueWZyaG1meHFpY2RodHR4a2lqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NjYzNDY3NywiZXhwIjoyMDAyMjEwNjc3fQ.xKmXdHqvIow96RjRvzX1ckYTeVZbjeVPfxkEBtE-8sM';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-const { data, error } = await supabase
-  .from('Calculator1')
-  .select()
-console.log(supabase.data);
+console.log('Supabase Instance: ', supabase)
