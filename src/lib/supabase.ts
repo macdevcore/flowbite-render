9c0ad65b70114256f87c328995d15e7ff88d36e0
MacDevCore <dechmann@datora.de>
2023-06-14 22:55:06 +0200

    2

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://anyfrhmfxqicdhttxkij.supabase.co';
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFueWZyaG1meHFpY2RodHR4a2lqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NjYzNDY3NywiZXhwIjoyMDAyMjEwNjc3fQ.xKmXdHqvIow96RjRvzX1ckYTeVZbjeVPfxkEBtE-8sM';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-console.log('Supabase Instance: ', supabase)
+export const userId = '77173b116fde045e661a0cb7ec902919';
+const supabaseUrl = 'https://mhzckigyfxwlxlpunqez.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1oemNraWd5Znh3bHhscHVucWV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY3NzMyNjcsImV4cCI6MjAwMjM0OTI2N30.MEWYY5XSrYFY8iUd9p_ybUlZ5IVAD95Y4Std-LYH-iM';
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+	global: { fetch: fetch.bind(globalThis) }
+});
