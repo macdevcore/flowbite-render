e2b31df8d73c266551ddf1f0ad9c7647e1880f11
MacDevCore <dechmann@datora.de>
2023-06-14 04:58:37 +0200

    d

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,11 +1,11 @@
 // NOTE: This is where you could wire up your own data providers:
 // GraphQL, Databases, REST APIs, CDNs, proxies, S3, Matrix, IPFS, you name it…
 
-import { API_URL, REMOTE_ASSETS_BASE_URL } from '../app/constants.js';
+import { API_URL, REMOTE_ASSETS_BASE_URL, MAIN_URL } from '../app/constants.js';
 
 // NOTE: These helpers are useful for unifying paths, app-wide
 export function url(path = '') {
-	return `${import.meta.env.SITE}${import.meta.env.BASE_URL}${path}`;
+	return `${MAIN_URL}/${path}`;
 }
 
 // TODO: Remove old local assets from git history (to make cloning snappier).
