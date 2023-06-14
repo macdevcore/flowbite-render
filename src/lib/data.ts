54c9aeedf698a309d3ee24531b81572c2e1dbe63
MacDevCore <dechmann@datora.de>
2023-06-14 03:40:28 +0200

    last try

---
+++ src/lib/data.ts
@@ -0,0 +1,15 @@
+// NOTE: This is where you could wire up your own data providers:
+// GraphQL, Databases, REST APIs, CDNs, proxies, S3, Matrix, IPFS, you name it…
+
+import { API_URL, REMOTE_ASSETS_BASE_URL } from '../app/constants.js';
+
+// NOTE: These helpers are useful for unifying paths, app-wide
+export function url(path = '') {
+	return `${import.meta.env.SITE}${import.meta.env.BASE_URL}${path}`;
+}
+
+// TODO: Remove old local assets from git history (to make cloning snappier).
+export function asset(path: string) {
+	// NOTE: Fetching remote assets from the Hugo admin dashboard Vercel dist.
+	return `${REMOTE_ASSETS_BASE_URL}/${path}`;
+}
