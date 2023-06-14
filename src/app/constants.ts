dcf585352fa2effd058607f026fc33c6a7f241a6
MacDevCore <dechmann@datora.de>
2023-06-14 06:41:26 +0200

    f

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -2,6 +2,7 @@ export const API_URL = `${import.meta.env.SITE}${import.meta.env.BASE_URL}api/`;
 
 export const REMOTE_ASSETS_BASE_URL = `https://flowbite-admin-dashboard.vercel.app`;
 export const MAIN_URL  = 'https://datora.onrender.com';
+export const USER_ID  = '77173b116fde045e661a0cb7ec902919';
 
 export const SITE_TITLE = 'DevCode Astro App';
 
